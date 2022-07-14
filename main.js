65ed36448989dbc95d43926952dad19ff96687aa
Rob DiVincenzo <rob.divincenzo@gmail.com>
2022-07-14 17:31:32 -0400

    Code cleanup
    Re-label button to Create New Grid
    Move callbacks into named functions

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
+/* Run JS */
 const container = document.querySelector('div#container');
 const customLayoutButton = document.querySelector('button#custom-layout-prompt');
 calculateGrid();
 
-customLayoutButton.addEventListener('click', function(e) {
-    let newSize = Number(prompt("Please enter a new size of your grid (i.e. 64 for a 64x64 grid, up to a maximum of 100 for a 100x100 grid)"));
-    if( isNaN(newSize) || newSize > 100 ) {
-        newSize = 16;
-    }
-    calculateGrid(newSize);
-});
+customLayoutButton.addEventListener('click', newGridPrompt);
 
 
+/* Functions */
 function calculateGrid( size=16 ) {
     clearGrid();
     for( let i = 0; i < size*size; i++ ){
@@ -19,7 +15,7 @@ function calculateGrid( size=16 ) {
         gridItem.addEventListener('mouseover', colorTile);
         container.appendChild(gridItem);
         container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    }    
+    } 
 }
 
 function clearGrid() {
@@ -28,4 +24,12 @@ function clearGrid() {
 
 function colorTile(e) {
     this.style.backgroundColor = "salmon";
+}
+
+function newGridPrompt(e){
+    let newSize = Number(prompt("Please enter a new size of your grid (i.e. 64 for a 64x64 grid, up to a maximum of 100 for a 100x100 grid)"));
+    if( isNaN(newSize) || newSize > 100 ) {
+        newSize = 16;
+    }
+    calculateGrid(newSize);
 }
